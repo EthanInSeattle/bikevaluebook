32dec0d3d37c4e11e5b365924ad5599d95dea8ef
ethancui <ethancui@ethancuis-iMac.local>
2019-08-06 13:26:01 -0700

    updated spider

--- crawler/bike_value_book/bike_value_book/spiders/price_spider.py
+++ crawler/bike_value_book/bike_value_book/spiders/price_spider.py
@@ -1,88 +1,53 @@
 import scrapy
+import re
 
 class PriceSpider(scrapy.Spider):
+    # name of the spider, used when invoking the spider through command line
     name = "price"
-    # start_urls = [
-    #     #'./price-1.html'
-    #     #'https://www.bicyclebluebook.com/SearchListingDetail.aspx?id=3068276&make=683&model=61546'
-    #     #'https://www.bicyclebluebook.com/SearchListingDetail.aspx?id=3077484&make=672&model=92114'
-    #     #'https://www.bicyclebluebook.com/SearchListing.aspx?make=683&model=61546'
-    #     'https://www.bicyclebluebook.com/SearchBikes.aspx'
-    # ]
 
-    # def start_requests(self):
-    #     urls = [
-    #         # 'http://quotes.toscrape.com/page/1/',
-    #         # 'http://quotes.toscrape.com/page/2/',
-    #         'https://www.bicyclebluebook.com/SearchListingDetail.aspx?id=3061293&make=672&model=82960'
-    #     ]
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
-
-    # def parse(self, response):
-    #     rawMakeLinks = response.css("a::attr(href)").getall()
-    #     cleanMakeLinks = filter(lambda x: "BicycleDatabase.aspx" in x, rawMakeLinks)
-    #     cleanMakeLinks = map(lambda x : 'https://www.bicyclebluebook.com' + x, cleanMakeLinks)
-    #     for makeLink in cleanMakeLinks:
-
-    # def start_request(self):
-    #     yield scrapy.Request(url='https://www.bicyclebluebook.com/SearchBikes.aspx', callback=self.parse_makes)
-
-    # def parse_makes(self, response):
+    # starting point of the spider, use self.parse as callback by default
     start_urls=['https://www.bicyclebluebook.com/SearchBikes.aspx']
+
+    # extract links of all makes
     def parse(self, response):
         rawMakeLinks = response.css("a::attr(href)").getall()
         cleanMakeLinks = filter(lambda x: "BicycleDatabase.aspx" in x, rawMakeLinks)
         cleanMakeLinks = map(lambda x: 'https://www.bicyclebluebook.com' + x, cleanMakeLinks)
         for makeLink in cleanMakeLinks:
-            # yield{'makelink': makeLink}
-            # print(makeLink)
             yield scrapy.Request(url=makeLink, callback=self.parse_models)
 
+    # extract links of all models
     def parse_models(self, response):
         rawModelLinks = response.css("a::attr(href)").getall()
         cleanModelLinks = filter(lambda x: "SearchListing.aspx" in x, rawModelLinks)
         cleanModelLinks = map(lambda x: 'https://www.bicyclebluebook.com' + x, cleanModelLinks)
         for modelLink in cleanModelLinks:
-            print(modelLink)
             yield scrapy.Request(url=modelLink, callback=self.parse_years)
     
+    # extract year, make, model, category, and link of all years
     def parse_years(self, response):
-        rawYearLinks = response.css("a::attr(href)").getall()
-        cleanYearLinks = filter(lambda x: "SearchListingDetail.aspx" in x, rawYearLinks)
-        cleanYearLinks = map(lambda x: 'https://www.bicyclebluebook.com' + x, cleanYearLinks)
-        for yearLink in cleanYearLinks:
-            yield scrapy.Request(url=yearLink, callback=self.parse_bike)
+        rawContent = response.css(".values-line").getall()
+        for content in rawContent:
+            #make = content.css(".col-sm-2").get()
+            year = re.search(r'<div class="col-sm-1 col-xs-1">(.*?)</div>', content).group(1)
+            make = re.search(r'<div class="col-sm-2 col-xs-3">(.*?)</div>', content).group(1)
+            model = re.search(r'<div class="col-sm-3 col-xs-3">(.*?)</div>', content).group(1)
+            category = re.search(r'<div class="col-sm-2 col-xs-2 truncate">(.*?)</div>', content).group(1)
+            link = 'https://www.bicyclebluebook.com' + re.search(r'onclick="location.href=\'(.*?)">', content).group(1)
+            yield scrapy.Request(url=link, callback=self.parse_bike, cb_kwargs={'year':year, 'make':make, 'model':model, 'category':category})
 
-    def parse_bike(self, response):
-        #print(response)
-        # print(len(cleanMakeLinks))
-        # print(cleanMakeLinks)
-        # rawLinks = response.css("a::attr(href)").getall()
-        # cleanLinks = filter(lambda x: "SearchListingDetail" in x, rawLinks)
-        # #print("RAWRAWRAWRAWRAWRAWRAW", rawLinks)
-        
-        # print(cleanLinks)
-        # if not cleanLinks:  # detail bike price page
-        #     # last
-        # else:
-        #     next_page = 
-
+    # extract bike price evaluation and description
+    def parse_bike(self, response, year, make, model, category):
         prices=[]
         for price in response.css(".col-xs-6::text").getall():
             if '$' in price:
                 clean = price.strip().replace('\r\n', '').replace(' ', '').replace('\'', '')
                 prices.append(clean)
-                yield{
-                    'price': clean
-                }
-        res = {}
+        res = {'year':year, 'make':make, 'model':model, 'category':category}
         for i in range(len(prices)):
             if i < len(prices)-1:
                 minPrice = prices[i].split('-')[0].replace('$', '')
                 maxPrice = prices[i].split('-')[1].replace('$', '')
-                #minPrice = int(minPrice)
-                #maxPrice = int(maxPrice)
                 if i == 0:
                     res["excellent"] = (minPrice, maxPrice)
                 elif i == 1:
@@ -93,7 +58,7 @@ class PriceSpider(scrapy.Spider):
                     res['fair'] = (minPrice, maxPrice)
             else:
                 price = prices[i].replace(',', '').replace('$', '')
-                #price = int(price)
                 res['msrp'] = (price)
-        print(res)
-
+        res['img'] = response.css('.col-sm-12 img::attr(src)').get()
+        res['raw-description'] =  response.css(".bvg-product-details").get()
+        yield res
